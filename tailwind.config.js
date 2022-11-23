845fd499aa30f65f1c9f02b49c2eff2a5383968a
fawaztoto <fawaztotonchy2@gmail.com>
2022-11-23 16:38:53 +0800

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
     extend: {
       screens:{
         "3xl": "2000px"
-      },
-    },
-  },
+      }, 
+    }, 
+  }, 
   plugins: [require("tailwind-scrollbar-hide")],
-}
+} 
