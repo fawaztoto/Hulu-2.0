bf84cfa821888cbca61751e9af7cfc20b76c2b54
fawaztoto <fawaztotonchy2@gmail.com>
2022-11-23 16:39:23 +0800

    done

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -11,9 +11,9 @@ const Thumbnail= forwardRef(({ result }, ref) => {
           `${BASE_URL}${result.backdrop_path || result.poster_path}`
            || `${BASE_URL}${result.poster_path}`
         } 
-        height={1080} 
-        width={1920}
-        />
+        height={1080}  
+        width={1920} 
+        />   
         <div className="p-2">
           <p className="truncate max-w-md">{result.overview}</p>
           <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
